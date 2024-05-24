e047210e428155c78e870833445fb453a6c8688f
Maria Soria <maria.it3@gmail.com>
2024-05-24 16:02:36 +0100

    lazy class smell: inline method to get rid of the not-needed class

--- src/dispensables/lazy-class/farmer.ts
+++ src/dispensables/lazy-class/farmer.ts
@@ -1,12 +1,10 @@
-import {ProductHandler} from './product-handler'
 import {Animal} from './animal'
 import {Product} from './product'
 
 export class Farmer {
-  productHandler: ProductHandler = new ProductHandler()
   inventory: Product[] = []
 
   pickUpProductFrom(animal: Animal) {
-    this.inventory.push(...this.productHandler.pickUpProductFrom(animal))
+    this.inventory.push(...(animal.pickUp()))
   }
 }
--- src/dispensables/lazy-class/product-handler.ts
+++
@@ -1,8 +0,0 @@
-import {Animal} from './animal'
-import {Product} from './product'
-
-export class ProductHandler {
-  pickUpProductFrom(animal: Animal): Product[] {
-    return animal.pickUp()
-  }
-}
